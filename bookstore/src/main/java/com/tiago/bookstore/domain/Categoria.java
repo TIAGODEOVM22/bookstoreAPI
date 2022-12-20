5c7d11289a37150e82f0fc98602e0d205b826861
Tiago de Oliveira <tiagodeoliveiravillalva@gmail.com>
2022-12-20 18:42:52 -0300

    Finalizando anotacoes com javax.persistence.

--- bookstore/src/main/java/com/tiago/bookstore/domain/Categoria.java
+++ bookstore/src/main/java/com/tiago/bookstore/domain/Categoria.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,10 +21,13 @@ import lombok.NoArgsConstructor;
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
 	
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livro = new ArrayList<>();
 	
 
