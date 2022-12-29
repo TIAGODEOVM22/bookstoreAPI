2246623ad7d995a1a49873f0cfe1ad0783023346
Tiago <tiagodeoliveiravillalva@gmail.com>
2022-12-29 07:34:09 -0300

    Classe DataIntegrityViolation estava com construtor duplicado após o MERGE com a branch_endpoint. CORRIGIDO

--- bookstore/src/main/java/com/tiago/bookstore/services/exception/DataIntegrityVaiolationException.java
+++ bookstore/src/main/java/com/tiago/bookstore/services/exception/DataIntegrityVaiolationException.java
@@ -1,7 +1,6 @@
 package com.tiago.bookstore.services.exception;
 
-<<<<<<< HEAD
-public class DataIntegrityVaiolationException extends RuntimeException{
+public class DataIntegrityVaiolationException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public DataIntegrityVaiolationException(String message, Throwable cause) {
@@ -14,21 +13,4 @@ public class DataIntegrityVaiolationException extends RuntimeException{
 		// TODO Auto-generated constructor stub
 	}
 
-	
-=======
-public class DataIntegrityVaiolationException extends RuntimeException {
-
-	public DataIntegrityVaiolationException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
-
-	public DataIntegrityVaiolationException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
-
->>>>>>> refs/heads/branch_endpoints
-	
-	
 }
